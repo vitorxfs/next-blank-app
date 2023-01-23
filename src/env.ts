9af1ba1d8a3f01e4f850c3af1b8c3a43062f86ca
Vitor Sanches <viisanches98@gmail.com>
2023-01-23 16:24:09 -0300

    feat: add SEO to _app component

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 // #region Public Keys
 export const GOOGLE_ANALYTICS_KEY = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY;
-export const WEBSITE_URL = process.env.WEBSITE_URL || '';
+export const WEBSITE_URL = process.env.WEBSITE_URL || 'localhost:3000';
 // #endregion Public Keys
 
 // #region Private Keys
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '@styles/globals.scss';
+import { SEO } from '@/config/SEO';
 import Analytics from '@config/Analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <SEO />
+      </Head>
+
       <Analytics />
+
       <Component {...pageProps} />
     </>
   );
